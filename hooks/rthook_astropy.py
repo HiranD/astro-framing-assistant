6124a998b1e238cd362847f7d0ddafe0aa9f8af9
HiranD <hiranhsw@gmail.com>
2026-04-09 22:09:53 +0200

    fix(build): use 'ignore' not 'silent' for iers_degraded_accuracy
    The 'silent' value is not valid for astropy's iers_degraded_accuracy
    config option. Only 'warn' and 'ignore' are accepted, causing the
    runtime hook to crash on startup in the frozen app.

--- hooks/rthook_astropy.py
+++ hooks/rthook_astropy.py
@@ -4,4 +4,4 @@ from astropy.utils.iers import conf as iers_conf
 
 iers_conf.auto_download = False
 iers_conf.auto_max_age = None
-iers_conf.iers_degraded_accuracy = 'silent'
+iers_conf.iers_degraded_accuracy = 'ignore'
